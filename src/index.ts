6f458b9c658230e38b68d59cf76d0507c2bfb17b
manaslaud <manaslaud@hotmail.com>
2023-07-17 12:42:27 +0530

    added camera, target etc

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ import {
     AnisotropyPlugin,
     GammaCorrectionPlugin,
 
-    addBasePlugins,
+    // addBasePlugins,
     ITexture, TweakpaneUiPlugin, AssetManagerBasicPopupPlugin, CanvasSnipperPlugin,
 
     IViewerPlugin,
@@ -25,24 +25,29 @@ import {
     // Texture, // Import THREE.js internals
 } from "webgi";
 import "./styles.css";
-
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+    gsap.registerPlugin(ScrollTrigger)
 async function setupViewer(){
 
     // Initialize the viewer
     const viewer = new ViewerApp({
         canvas: document.getElementById('webgi-canvas') as HTMLCanvasElement,
+        useRgbm:false
     })
 
     // Add some plugins
     const manager = await viewer.addPlugin(AssetManagerPlugin)
-
+    const camera=viewer.scene.activeCamera;
+    const positon=camera.position 
+    const target=camera.target
     // Add a popup(in HTML) with download progress when any asset is downloading.
     await viewer.addPlugin(AssetManagerBasicPopupPlugin)
 
     // Add plugins individually.
-    // await viewer.addPlugin(GBufferPlugin)
-    // await viewer.addPlugin(new ProgressivePlugin(32))
-    // await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
+    await viewer.addPlugin(GBufferPlugin)
+    await viewer.addPlugin(new ProgressivePlugin(32))
+    await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
     // await viewer.addPlugin(GammaCorrectionPlugin)
     // await viewer.addPlugin(SSRPlugin)
     // await viewer.addPlugin(SSAOPlugin)
@@ -56,7 +61,7 @@ async function setupViewer(){
     // and many more...
 
     // or use this to add all main ones at once.
-    await addBasePlugins(viewer)
+    // await addBasePlugins(viewer)
 
     // Add more plugins not available in base, like CanvasSnipperPlugin which has helpers to download an image of the canvas.
     await viewer.addPlugin(CanvasSnipperPlugin)
@@ -74,7 +79,33 @@ async function setupViewer(){
     const uiPlugin = await viewer.addPlugin(TweakpaneUiPlugin)
     // Add plugins to the UI to see their settings.
     uiPlugin.setupPlugins<IViewerPlugin>(TonemapPlugin, CanvasSnipperPlugin)
+    function scrollAnimate(){
+        const tl=gsap.timeline()
+        tl.to(positon,{x:3.3930950568,y:-0.546519012,z:-6.6265360939,
+     duration:4,scrollTrigger:{
+        trigger:'.section2',start:'top bottom', end:"top top",scrub:true,markers:true
+     }  ,onUpdate })
+     .to(target,{x:0,z:0,y:0,
+        duration:4,scrollTrigger:{
+           trigger:'.section2',start:'top bottom', end:"top top",scrub:true,markers:true
+        }  ,onUpdate })
+    }
+    scrollAnimate()
 
+    let needsUpdate=true;
+    function onUpdate(){
+        needsUpdate=true;
+        viewer.renderer.resetShadows();
+
+    }
+    viewer.addEventListener('preFrame',()=>{
+        if(needsUpdate){
+            camera.positionUpdated(true)
+            camera.targetUpdated(true)
+            needsUpdate=false;
+
+        }
+    })
 }
 
 setupViewer()
